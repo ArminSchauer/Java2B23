16d619a99f5d88a5cc2508b4dcf95db0ad80e4b9
HTLAccount <20220149@htlstp.at>
2024-03-21 12:54:18 +0100

    Complexe Zahlen - 19.03.2024

--- src/at/ac/htlstp/et/sj23/k2b/oop/Complex.java
+++ src/at/ac/htlstp/et/sj23/k2b/oop/Complex.java
@@ -5,14 +5,20 @@ package at.ac.htlstp.et.sj23.k2b.oop;
  */
 public class Complex {
 
-    /** Realteil der Zahl */
-    private double re;
+    private static final double TOLERANZ = 1e-6;
+    /**
+     * Realteil der Zahl
+     */
+    private final double re;
 
-    /** Imaginärteil einer Zahl */
-    private double im;
+    /**
+     * Imaginärteil einer Zahl
+     */
+    private final double im;
 
     /**
      * Erzeugt eine komplexe Zahl aus Real und Imaginärteil
+     *
      * @param re Realteil
      * @param im Imaginärteil
      */
@@ -23,6 +29,7 @@ public class Complex {
 
     /**
      * Erzeugt eine komplexe Zahl mit Realteil und Imaginärteil 0
+     *
      * @param re Realteil
      */
     public Complex(double re) {
@@ -31,19 +38,23 @@ public class Complex {
 
     /**
      * Erzeugt eine
-     * @param abs Betrag
-     * @param arg Winkel
+     *
+     * @param abs     Betrag
+     * @param arg     Winkel
      * @param radiant true für Rad und false für Grad
      */
     public Complex(double abs, double arg, boolean radiant) {
         //this(abs * Math.cos(arg * (radiant?1:Math.PI/180)), abs * Math.sin(arg * (radiant?1:Math.PI/180)));
-        if(!radiant) {arg = arg * Math.PI/180;}
+        if (!radiant) {
+            arg = arg * Math.PI / 180;
+        }
         re = abs * Math.cos(arg);
         im = abs * Math.sin(arg);
     }
 
     /**
      * Statischer Konstruktor der eine komplexe Zahl und einen Winkel erzeugt
+     *
      * @param abs Betrag
      * @param arg Winkel in Rad
      * @return Complexe Zahl
@@ -59,5 +70,84 @@ public class Complex {
         return re + " + " + im + "i";
     }
 
+    public double getRe() {
+        return re;
+    }
+
+    public Complex setRe(double re) {
+        return new Complex(re, im);
+    }
+
+    public double getIm() {
+        return im;
+    }
+
+    public Complex setIm(double im) {
+        return new Complex(re, im);
+    }
+
+    public double getAbs() {
+        return Math.sqrt(re * re + im * im);
+    }
+
+    public double getArg() {
+        return Math.atan2(im, re);
+    }
+
+    public Complex setAbs(double abs) {
+        double v = abs / getAbs();
+        return new Complex(re * v, im * v);
+    }
+
+    /*******************************************************************************************************************
+     * Berechnungsmethoden
+     ******************************************************************************************************************/
+
+    public Complex add(Complex z) {
+        return new Complex(re + z.re, im + z.im);
+    }
+
+    public Complex add(double r) {
+        // return new Complex(re + r, im);
+        return add(this, r);
+    }
+    public static Complex add(Complex z1, Complex z2) {
+        // return new Complex(z1.re + z2.re, z1.im + z2.im);
+        return z1.add(z2);
+    }
+
+    public static Complex add(Complex z1, double r) {
+        return new Complex(z1.re + r, z1.im);
+    }
+
+    public Complex neg() {
+        return new Complex(-re, -im);
+    }
+
+    public Complex sub(Complex z) {
+        return add(z.neg());
+    }
+
+    public Complex mul(Complex z) {
+        return createVersor(getAbs() * z.getAbs(), getArg() + z.getArg());
+    }
+
+    public Complex mul1(Complex z) {
+        return  new Complex(re*z.re-im*z.im, re*z.im+im*z.re);
+    }
+
+    public Complex inv() {
+        double N = re*re+im*im;
+        return new Complex(re/N, -im/N);
+    }
+
+    public Complex div(Complex z) {
+        return mul(z.inv());
+    }
+
+    public boolean equals(Complex z) {
+        double s = Math.max(getAbs(), z.getAbs()) * TOLERANZ;
+        return (Math.abs(re-z.re) <s && Math.abs(im- z.im) <s);
+    }
 
 }
--- src/at/ac/htlstp/et/sj23/k2b/oop/TestComplex.java
+++ src/at/ac/htlstp/et/sj23/k2b/oop/TestComplex.java
@@ -3,8 +3,34 @@ package at.ac.htlstp.et.sj23.k2b.oop;
 public class TestComplex {
 
     public static void main(String[] args) {
-        Complex c = new Complex(2, 3);
+        Complex c = new Complex(2, 5);
+        Complex d;
+        d = c;
         System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        c = c.setAbs(3);
+        System.out.println("c = " + c);
+        System.out.println("abs:" + c.getAbs());
+        System.out.println("d = " + d);
+
+        // und jetzt addieren wir
+        Complex z1 = new Complex(2, 4);
+        Complex z2 = new Complex(1, 3);
+        c = z1.add(z2);
+        System.out.println(z1 + " + " + z2 + " = " + c);
+        c = z1.add(3);
+        System.out.println(z1 + " + 3 = " + c);
+        c = Complex.add(z1, z2);
+        c = z1.mul(z2);
+        d = z1.mul1(z2);
+        System.out.println(c + " = " + d);
+        c = z1.div(z2);
+        d = c.mul1(z2);
+        System.out.println(c + " = " + d);
+
+        if(z1.equals(d)) {
+            System.out.println(z1 + " & " + d + " sind gleich groß!");
+        }
     }
 
 }
